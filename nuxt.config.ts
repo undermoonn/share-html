c2e88867726b9b88ece91aa224dc65d8aed5d204
undermoonn <under-moon@outlook.com>
2024-06-15 04:47:06 +0800

    feat: description meta

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,13 @@ export default defineNuxtConfig({
           rel: 'shortcut icon',
           href: '/icon.png'
         }
+      ],
+      meta: [
+        {
+          name: 'description',
+          content:
+            'Online HTML Editor. You can edit HTML, CSS and JavaScript code, and view the result in your browser immediately, and share to others.'
+        }
       ]
     }
   },
